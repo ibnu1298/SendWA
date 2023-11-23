b9f83c9225387e4cbe9f351ec3591f9fff256d8e
Ibnu Aqil Al Barohin <kalangkang1298@gmail.com>
2023-11-23 16:58:39 +0700

    editpackage

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const serverless = require('serverless-http')
 const app = express()
 const port = process.env.port || 3000
 const router = require("./Router/wa")
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
-app.use("/", router)
+app.use("/.netlify/functions/api", router)
 
 app.listen(port, () => {
   console.log(`Server app listening on port ${port}`)
 })
+
+module.exports.handler  = serverless(app)
